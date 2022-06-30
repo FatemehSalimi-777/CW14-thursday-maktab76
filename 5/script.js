96732e43b9c3ff5a43b3e96eb59f4363856a54d5
FatemehSalimi-777 <fsalimi777@gmail.com>
2022-06-30 16:19:46 +0430

    q5 solved

---
+++ 5/script.js
@@ -0,0 +1,15 @@
+$('.child').each(function(){
+    $(this).click(function(){
+        $(this).text('Hello');
+    });
+})
+
+
+$('.child').each(function(){
+    $(this).dblclick(function(){
+        $(this).text('Goodbye');
+    });
+})
+
+
+
